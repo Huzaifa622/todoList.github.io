f8becfd54f89285d6d41a27a77018e1c9fdebf69
huzaifa622 <thuzaifa07@gmail.com>
2023-04-29 23:03:36 +0500

    first react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {useState} from "react";
 
 function App() {
+const [todoList, settodoList]=useState([])
+const [newTask, setnewTask]=useState('')
+
+const inputType = (e) =>{
+setnewTask(e.target.value)
+}
+const addTodoList = ()=>{
+  const task ={
+    id: todoList.length === 0 ? 1 : todoList[todoList.length -1].id +1,
+    taskName : newTask,
+    Completed : false,
+  };
+  settodoList([...todoList , task]);
+}
+
+const delTask = (id) =>{
+let newList = todoList.filter((tasks) => tasks.id !== id )
+settodoList(newList);
+
+}
+
+const completeTask =(id)=>{
+settodoList(todoList.map((task)=>{
+  if(task.id === id){
+    return {...task , Completed : !task.Completed}
+  }else{
+    return task
+  }
+
+
+}));
+
+};
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='inputTask'>
+        <input onChange={inputType}/>
+        <button onClick={addTodoList}>Add Task</button>
+      </div>
+      <div className='todoList'>
+         {todoList.map((tasks)=>{
+       return ( 
+       <div style={tasks.Completed === true ? {backgroundColor: 'red'} : {backgroundColor: 'white'}}>
+        <h1>{tasks.taskName}</h1>
+        <button onClick={()=> completeTask(tasks.id)}>Completed</button>
+        <button onClick={()=> delTask(tasks.id)} >X</button>
+        
+        </div>
+       );
+       })}
+       
+      </div>
     </div>
   );
-}
+};
+
+
 
 export default App;
